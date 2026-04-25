8dc3c26b73fe9bbc5bd4957996db3b687f51597c
InforusInferno <inforus.yt@gmail.com>
2026-04-25 14:01:17 +0300

    attempt 2

--- cogs/music.py
+++ cogs/music.py
@@ -31,6 +31,11 @@ ytdl_format_options = {
     'youtube_include_dash_manifest': False,
     'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
     'referer': 'https://www.youtube.com/',
+    'extractor_args': {
+        'youtube': {
+            'player_client': ['android', 'ios']
+        }
+    },
 }
 
 
